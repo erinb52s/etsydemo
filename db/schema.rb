67a6c314b11e19f147673510087cbf67ee013516
erinb52s <erinb52s@gmail.com>
2015-05-14 15:31:35 -0700

    Add order history pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514173055) do
+ActiveRecord::Schema.define(version: 20150514213614) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150514173055) do
     t.integer  "listing_id"
     t.integer  "buyer_id"
     t.integer  "seller_id"
+    t.string   "zipcode"
   end
 
   create_table "users", force: :cascade do |t|
