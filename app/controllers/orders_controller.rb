014620d7bedf75fc846956ea788b8024ead29ac4
erinb52s <erinb52s@gmail.com>
2015-05-17 13:06:47 -0700

    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,8 +17,6 @@ class OrdersController < ApplicationController
     @listing = Listing.find(params[:listing_id])
   end
 
-
-
   # POST /orders
   # POST /orders.json
   def create
@@ -43,6 +41,12 @@ class OrdersController < ApplicationController
     rescue Stripe::CardError => e
       flash[:danger] = e.message
     end
+
+    transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
 
     respond_to do |format|
       if @order.save
